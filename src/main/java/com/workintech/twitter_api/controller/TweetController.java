18cc88701dc6d7573622ced3079c675c617fa5b3
Efe Cihan Zengin <efecihanzengin@gmail.com>
2025-06-26 14:32:33 +0300

    Tweet CRUD operations active and working

--- src/main/java/com/workintech/twitter_api/controller/TweetController.java
+++ src/main/java/com/workintech/twitter_api/controller/TweetController.java
@@ -1,4 +1,41 @@
 package com.workintech.twitter_api.controller;
 
+
+import com.workintech.twitter_api.entity.Tweet;
+import com.workintech.twitter_api.service.TweetService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/tweet")
 public class TweetController {
+
+    private final TweetService tweetService;
+
+    @GetMapping //tum tweetleri getirme
+    public Iterable<Tweet> getAllTweets() {
+        return tweetService.findAll(); //TweetServicedeki tum tweetleri alip dondur.
+    }
+
+    @PostMapping // yeni bir tweet olusturan
+    public Tweet createTweet(@RequestBody Tweet tweet) {
+        return tweetService.save(tweet); //tweetservicedeki tweeti kaydedip dondur.
+    }
+
+    @GetMapping("/{id}") //belirli id ye sahip twiti getiren
+    public Tweet getTweetById(@PathVariable Long id) {
+        return tweetService.findById(id); //tweetserviceden id ye gore tweet bulup dodur
+    }
+
+    @PutMapping("/{id}") //guncelleme
+    public Tweet updateTweet(@PathVariable Long id, @RequestBody Tweet tweet) {
+        tweet.setId(id);
+        return tweetService.save(tweet); // RequestBody ile json formatinda nesneyi aldik, id yi set edip guncelleyip dondurduk
+    }
+
+    @DeleteMapping("/{id}") //silme
+    public void deleteTweet(@PathVariable Long id) {
+        tweetService.delete(id);
+    }
 }
